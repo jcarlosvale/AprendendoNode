7a23e009772dbf135156bed987d9ab6c5a2dd8a0
Joao Carlos Sousa do Vale <joaocarlos.sousadovale@tipico.com>
2022-06-01 16:37:20 +0200

    'server'

--- src/main.js
+++ src/main.js
@@ -1,16 +1,50 @@
-import {createServer} from 'http';
+import { createServer} from 'http';
+import { readFile } from 'fs';
+import { resolve } from 'path';
 
 const server = createServer((req, res) => {
     switch (req.url) {
         case '/status': {
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.write('OK');
+            res.writeHead(200, {'Content-Type': 'text/json'});
+            res.write(
+                JSON.stringify({
+                    status: 'ok'
+                })
+            );
+            res.end();
+            return
+        }
+        case '/sign-in': {
+            const filePath = resolve(__dirname, './pages/sign-in.html');
+            readFile(filePath, (err, data) => {
+                if (err) {
+                    res.writeHead(500, {'Content-Type': 'text/plain'});
+                    res.write(err.message);
+                    res.end();
+                    return
+                }
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                res.end();
+            });
+            return;
+        } 
+        case '/authenticate': {
+            return;
+        } 
+        default: {
+            res.writeHead(404, 'Service Not Found');
             res.end();
             return
         }
     }
 });
 
-server.listen(8000, '127.0.0.1', () => {
-        console.log('Server listening on http://127.0.0.1:8000');
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
+
+
+server.listen(PORT, HOSTNAME, () => {
+        console.log(`Server listening on http://${HOSTNAME}:${PORT}`);
 });
