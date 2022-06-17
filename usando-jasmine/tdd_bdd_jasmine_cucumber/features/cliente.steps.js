ff273751281bd4f1128934571e0615d021897c4c
Joao Carlos Sousa do Vale <joaocarlos.sousadovale@tipico.com>
2022-06-17 16:50:59 +0200

    cucumber test OK

--- usando-jasmine/tdd_bdd_jasmine_cucumber/features/cliente.steps.js
+++ usando-jasmine/tdd_bdd_jasmine_cucumber/features/cliente.steps.js
@@ -1,17 +1,26 @@
 const assert = require('assert');
 const {Given, When, Then} = require('cucumber');
 
+const path = require('path');
+const {ServiceBuilder} = require('selenium-webdriver/chrome');
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+const pathDriver = "/Pessoal/Projects/AprendendoNode/usando-jasmine/tdd_bdd_jasmine_cucumber/driver/chromedriver.exe"
+const serviceBuilder = new ServiceBuilder(pathDriver);
+
+const driver = new Builder().forBrowser('chrome').setChromeService(serviceBuilder).build();
+
 Given('I\'m have {string} clients in my database', function (string) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    console.log("Dados inseridos no BD")
 });
 
-When('I access the home webpage', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+When('I access the home webpage', async () => {
+    await driver.get('http://localhost:3000/')
+    await driver.sleep(1000)
 });
 
-Then('See the List of {string} items', function (string) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+Then('See the List of {string} items', async function (string) {
+    let qtd = await driver.findElements(By.css('tbody tr'))
+    assert.equal(qtd.length, parseInt(string))
+    await driver.quit()
 });
--- usando-jasmine/tdd_bdd_jasmine_cucumber/models/cliente.js
+++ usando-jasmine/tdd_bdd_jasmine_cucumber/models/cliente.js
@@ -1,9 +1,9 @@
 module.exports = class Cliente{
     
-    constructor() {
-        this.id = 0
-        this.nome = "nome"
-        this.telefone = "telefone"
+    constructor(id = 0, nome="", telefone="") {
+        this.id = id
+        this.nome = nome
+        this.telefone = telefone
     
     }
 
@@ -13,16 +13,16 @@ module.exports = class Cliente{
     
     static todos() {
         return [
-            new Cliente(),
-            new Cliente(),
-            new Cliente(),
-            new Cliente(),
-            new Cliente(),
-            new Cliente(),
-            new Cliente(),
-            new Cliente(),
-            new Cliente(),
-            new Cliente()
+            new Cliente(1, "Zezim 1", "123456789"),
+            new Cliente(2, "Zezim 2", "123456789"),
+            new Cliente(3, "Zezim 3", "123456789"),
+            new Cliente(4, "Zezim 4", "123456789"),
+            new Cliente(5, "Zezim 5", "123456789"),
+            new Cliente(6, "Zezim 6", "123456789"),
+            new Cliente(7, "Zezim 7", "123456789"),
+            new Cliente(8, "Zezim 8", "123456789"),
+            new Cliente(9, "Zezim 9", "123456789"),
+            new Cliente(10, "Zezim 10", "123456789")
         ]
     }
 
